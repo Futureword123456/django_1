d5e79a427b7ac53532070c9d0ba0883313f31fe2
Administrator <2635681517@qq.com>
2021-02-18 19:01:06 +0800

    建模

--- oauth/models.py
+++ oauth/models.py
@@ -20,7 +20,6 @@ class Course(CommonUtils):
         db_table = 'course'
 
 
-# Create your models here.
 class Student(CommonUtils):
     """学生表"""
     name = models.CharField(max_length=64, verbose_name='学生姓名')
@@ -41,8 +40,51 @@ class Student(CommonUtils):
 
     class Meta:
         """修改数据库的名称"""
-        db_table = 'students'
+        db_table = 'user'
         ordering = ['-updated_at']
+
+
+"""学生详细信息"""
+
+
+class UserDetail(models.Model):
+    user = models.OneToOneField(Student, on_delete=models.CASCADE)
+
+    sign = models.CharField('用户签名', max_length=256)
+    isonline = models.IntegerField('是否在线', choices=(
+        ('1', '是'),
+        ('2', '否'),
+        ('3', '离开'),
+        ('4', '忙碌'),
+        ('5', '隐身'),
+    ), default='1')
+    birthday = models.DateTimeField('生日')
+    bloodtype = models.IntegerField('血型', choices=(
+        ('1', 'A'),
+        ('2', 'B'),
+        ('3', 'O'),
+        ('4', 'AB'),
+        ('5', '其他')
+    ), default='1')
+    professional = models.TextField('职业', max_length=256)
+
+    class Meta:
+        db_table = 'user_detail'
+
+
+class UserAddress(CommonUtils):
+    """用户地址"""
+    user = models.ForeignKey(Student, verbose_name='学生',on_delete=models.CASCADE)
+    phone = models.CharField('收件人电话', max_length=11)
+    address = models.CharField('收件人地址', max_length=64)
+    zip_code = models.CharField('邮编', max_length=10, null=True, blank=True)
+    is_vaild = models.BooleanField('是否有效', default=True)
+
+    class Meta:
+        db_table = 'user_address'
+
+
+# Create your models here.
 
 
 class ProxyStudent(Student):
